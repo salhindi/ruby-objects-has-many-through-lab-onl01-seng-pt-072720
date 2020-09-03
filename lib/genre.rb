2ec10e0f883eb14e5fc072a10725860cf14231e4
Sara A <calisaraalhindi@gmail.com>
2020-09-03 02:46:33 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,19 +1,15 @@
 class Genre 
   attr_accessor :name 
   
-  @@all = []
+
   
   def initialize(name)
     @name=name
-    @@all << self
   end
   
   def name
     @name
   end
   
-  def self.all 
-    @@all 
-  end
-  
+
 end
