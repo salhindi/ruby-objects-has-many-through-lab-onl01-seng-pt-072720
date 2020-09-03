3cf1b15440bf99fbc5e60456497495d267b58685
Sara A <calisaraalhindi@gmail.com>
2020-09-03 02:32:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,18 @@
 class Artist
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name= name
+    @@all << self
   end
   
   def name
     @name
+  end
+  
+  def self.all 
+    @@all 
   end
 end
